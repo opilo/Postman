624cdb2c3dc4d20ac9c92f57a088a26b18624d16
miladr <miladr@gmail.com>
2014-09-29 00:29:02 +0330

    migration fixed

--- src/migrations/2014_09_24_230531_init.php
+++ src/migrations/2014_09_24_230531_init.php
@@ -18,6 +18,8 @@ class Init extends Migration {
             $table->bigInteger('uid');
             $table->string('token', 100);
             $table->string('url', 100);
+            $table->string('pattern', 500);
+            $table->string('group', 50);
             $table->tinyInteger('status')->default(0)->unsigned();
             $table->timestamp('created_at');
             $table->timestamp('updated_at')->nullable();
